62c6c6487e06020c2225c09d63c70ce50b454be1
Jorge Betancourt <jmbetancourt@google.com>
2026-04-15 13:03:43 -0400

    Reland "[skcapture] Add tracking content IDs to SkCapture"
    Can be landed after https://crbug.com/skia/506208928 resolves.
    
    This is a reland of commit c3d3126faf90e5a26f4b9885d1a4ae905dd8b158
    
    Original change's description:
    > [skcapture] Add tracking content IDs to SkCapture
    >
    > Content IDs are attached to a surface and track the state of the surface at various points throughout capture, primarily reads of a surface.
    >
    > This CL does the following:
    > * Updates our current demo capture example to have more draws and an extra makeImageSnapshot to test the content id generation.
    > * Refactors makeCaptureBreakpoint to return the content ID that should be recorded in capture.
    > * Add content tracking to all image draw calls through annotations.
    >
    > This CL tracks content IDs for images generated from makeImageSnapshot, but does not cover makeTemporaryImage.
    >
    > Change-Id: Ia05c05c5157d4eda15057869b787a2d51a59d32d
    > Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1196277
    > Reviewed-by: Greg Daniel <egdaniel@google.com>
    > Commit-Queue: Jorge Betancourt <jmbetancourt@google.com>
    
    Change-Id: I75052bb4a40e679e384e40d4f10d11f6b6fe3180
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1221676
    Commit-Queue: Jorge Betancourt <jmbetancourt@google.com>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>

--- src/image/SkSurface_Base.h
+++ src/image/SkSurface_Base.h
@@ -16,6 +16,8 @@
 #include "include/core/SkScalar.h"
 #include "include/core/SkSurface.h"
 #include "include/core/SkTypes.h"
+#include "src/capture/SkCaptureManager.h"
+#include "src/image/SkImage_Base.h"
 
 #include <cstdint>
 #include <memory>
@@ -170,11 +172,13 @@ public:
     // from SkSurface_Base).
     virtual sk_sp<const SkCapabilities> onCapabilities();
 
+    virtual uint32_t getPixelStorageID() const = 0;
+
     /**
      * If capturing, signals to the capture manager and capture canvas to break off the recording
      * SkPicture into a new SkPicture.
      */
-    void createCaptureBreakpoint();
+    SkContentID createCaptureBreakpoint();
 
     inline SkCanvas* getCachedCanvas();
     inline sk_sp<SkImage> refCachedImage();
@@ -225,9 +229,13 @@ sk_sp<SkImage> SkSurface_Base::refCachedImage() {
     if (fCachedImage) {
         return fCachedImage;
     }
-    this->createCaptureBreakpoint();
+    SkContentID contentID = this->createCaptureBreakpoint();
 
     fCachedImage = this->onNewImageSnapshot();
+    if (fCachedImage) {
+        as_IB(fCachedImage)->setDerivedSurfaceID(this->getPixelStorageID());
+        as_IB(fCachedImage)->setContentID(contentID);
+    }
 
     SkASSERT(!fCachedCanvas || fCachedCanvas->getSurfaceBase() == this);
     return fCachedImage;
